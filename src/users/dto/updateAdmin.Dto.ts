8559d43a25efac8940c793e4909fb3d535a91b95
Danijel Gajic <danijelgajic@Danijels-MacBook-Air.local>
2026-04-23 23:48:58 +0200

    fix prisma setup

--- src/users/dto/updateAdmin.Dto.ts
+++ src/users/dto/updateAdmin.Dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsEnum } from 'class-validator';
-import { Role } from '@prisma/client';
+import { Role } from 'generated/prisma/enums';
 
 export class UpdateRoleDto {
   @ApiProperty({
