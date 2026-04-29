3876bb254276ba0ef6ccebadb726a20be7d3e4db
Danijel Gajic <danijelgajic@Danijels-MacBook-Air.local>
2026-04-29 22:18:16 +0200

    recreate dto properly

--- src/products/dto/pagination.Dto.ts
+++ src/products/dto/pagination.Dto.ts
@@ -1,30 +1,37 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional, IsString, Min } from 'class-validator';
+import { IsIn, IsInt, IsOptional, IsString, Min } from 'class-validator';
 
 export class PaginationDto {
-  @ApiProperty({ example: '2', description: 'Page Number' })
+  @ApiPropertyOptional({ example: 1 })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number;
-  @ApiProperty({ example: '10', description: 'Load Limit' })
+
+  @ApiPropertyOptional({ example: 10 })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
   limit?: number;
-  @ApiProperty({ example: 'Macbook', description: 'Search By Name' })
+
+  @ApiPropertyOptional({ example: 'keyboard' })
   @IsOptional()
   @IsString()
   search?: string;
-  @IsString()
+
+  @ApiPropertyOptional({
+    example: 'name',
+    enum: ['name', 'price', 'stock', 'createdAt'],
+  })
   @IsOptional()
-  @ApiProperty({ example: '100', description: 'Sort by price ' })
-  sortBy?: string;
-  @ApiProperty({ example: 'asc', description: 'List  asc or desc ' })
+  @IsIn(['name', 'price', 'stock', 'createdAt'])
+  sortBy?: 'name' | 'price' | 'stock' | 'createdAt';
+
+  @ApiPropertyOptional({ example: 'asc', enum: ['asc', 'desc'] })
   @IsOptional()
-  @IsString()
-  order?: string;
+  @IsIn(['asc', 'desc'])
+  order?: 'asc' | 'desc';
 }
